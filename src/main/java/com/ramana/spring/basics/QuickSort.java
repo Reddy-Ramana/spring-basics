894507bb54761b03e7335c6574ceff56c255db57
Ramana R. A <ramana.prep@gmail.com>
2021-09-30 15:44:03 +0530

    Added @Primary annotation

--- src/main/java/com/ramana/spring/basics/QuickSort.java
+++ src/main/java/com/ramana/spring/basics/QuickSort.java
@@ -1,5 +1,10 @@
 package com.ramana.spring.basics;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSort implements Sorting {
 
 	public String sort(int[] numbers, int numberToSearch) {
