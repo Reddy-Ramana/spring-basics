5cd0d84404269c1df4693f55f30a892975c3163d
Ramana R. A <ramana.prep@gmail.com>
2021-10-04 16:40:53 +0530

    Updated with multiple changes

--- src/main/java/com/ramana/spring/basics/SimpleBinarySearch.java
+++ src/main/java/com/ramana/spring/basics/SimpleBinarySearch.java
@@ -1,5 +1,9 @@
 package com.ramana.spring.basics;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -9,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SimpleBinarySearch {
-
+   private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	@Qualifier("quick")
 	private Sorting sort;
@@ -17,6 +21,20 @@ public class SimpleBinarySearch {
 	public String binarySearchService(int[] Numbers, int result) {
 
 		return sort.sort(Numbers, result);
+	}
+	
+	@PostConstruct
+	public void postConstruct() {
+		
+		LOGGER.info("within Post construct");
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct1() {
+		
+		LOGGER.info("within Post construct1");
+		
 	}
 
 }
