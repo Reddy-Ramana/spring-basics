afe624aa15e08118c57a6567fb8ea6cb3620fe43
Ramana R. A <ramana.prep@gmail.com>
2021-10-04 10:41:57 +0530

    Code Cleanup

--- src/main/java/com/ramana/spring/basics/BubbleSort.java
+++ src/main/java/com/ramana/spring/basics/BubbleSort.java
@@ -1,7 +1,6 @@
 package com.ramana.spring.basics;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
