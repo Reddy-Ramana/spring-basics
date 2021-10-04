073d2a116572dfd0fe49be35fc3b589e0b655b2c
Ramana R. A <ramana.prep@gmail.com>
2021-10-04 10:38:18 +0530

    Updated POM.xml to maven 3.1.1 version to remove unknown error in pom.xml file

--- src/main/java/com/ramana/spring/basics/SpringbasicsApplication.java
+++ src/main/java/com/ramana/spring/basics/SpringbasicsApplication.java
@@ -10,6 +10,9 @@ public class SpringbasicsApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(SpringbasicsApplication.class, args);
 		SimpleBinarySearch binarySearch = applicationContext.getBean(SimpleBinarySearch.class);
+		SimpleBinarySearch binarySearch1 = applicationContext.getBean(SimpleBinarySearch.class);
+		System.out.println(binarySearch);
+		System.out.println(binarySearch1);
 		String result = binarySearch.binarySearchService(new int[] { 12 }, 23);
 		System.out.println(result);
 	}
